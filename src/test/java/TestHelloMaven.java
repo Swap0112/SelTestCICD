a767bd38d66f18101c35a6e87bf6d0c82d0503e8
Swap0112 <swapnilsalukhe@gmail.com>
2022-06-29 11:40:48 +1000

    first commit

---
+++ src/test/java/TestHelloMaven.java
@@ -0,0 +1,35 @@
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestHelloMaven {
+	
+	@Test
+	public void testHelloMaven()
+	
+	{
+	    System.out.println(" ..Hello World.. ");
+	    
+	    System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		
+		//WebDriver driver = new FirefoxDriver();
+		
+		driver.get("https://www.naukri.com/");
+		
+		//Implicit wait
+		
+		driver.manage().timeouts().implicitlyWait(12, TimeUnit.SECONDS);
+		
+		driver.close();
+	    
+	   						
+	}
+}
+	
+  	
+	
